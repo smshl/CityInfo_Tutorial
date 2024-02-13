66060ddb28d07320e6932ca6e7fcddbe0edde3bb
SMSHL <m.shojaee.langari@gmail.com>
2024-02-13 18:52:53 +0330

    working with file formats and content type, end of episode 24

--- CityInfo.Api/Program.cs
+++ CityInfo.Api/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.StaticFiles;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -9,6 +11,7 @@ builder.Services.AddControllers(options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<FileExtensionContentTypeProvider>();
 
 var app = builder.Build();
 
