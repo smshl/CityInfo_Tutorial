cf5bd9024b308b4d62b13a4b7bdb6c9577580709
SMSHL <m.shojaee.langari@gmail.com>
2024-02-14 09:44:38 +0330

    first post method, end of episode 26

---
+++ CityInfo.Api/Models/PointOfViewDtoForCreation.cs
@@ -0,0 +1,8 @@
+﻿namespace CityInfo.Api.Models
+{
+    public class PointOfViewDtoForCreation
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
